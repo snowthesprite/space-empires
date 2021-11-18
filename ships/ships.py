44782590d90f045dfba4070f9b46fe62a50caa30
snowthesprite <56653847+snowthesprite@users.noreply.github.com>
2021-11-18 23:20:35 +0000

    organized strats

--- ships/ships.py
+++ ships/ships.py
@@ -13,19 +13,17 @@ class Ship () :
                 'ship_id': att['id']}
         return ship_dict
 
-class Scout (Ship) :
+class Battleship (Ship) :
     def __init__(self, player_number, id) :
-        self.hp = 1
-        self.atk = 3
-        self.df = 0
+        self.hp = 3
+        self.atk = 5
+        self.df = 2
         self.pn = player_number
-        self.id = 'SC' + str(id)
-        self.type = 'E'
-        self.list_id = None
-        self.other = ['Scout', id, 'Ship']
+        self.id = 'BB' + str(id)
+        self.type = 'A'
+        #self.list_id = None
+        self.other = ['Battleship', id, 'Ship']
 
-
-    
 class BattleCruiser (Ship) :
     def __init__(self, player_number,id) :
         self.hp = 2
@@ -34,5 +32,62 @@ class BattleCruiser (Ship) :
         self.pn = player_number
         self.id = 'BC' + str(id)
         self.type = 'B'
-        self.list_id = None
+        #self.list_id = None
         self.other = ['BattleCruiser', id, 'Ship']
+
+class Cruiser (Ship) :
+    def __init__(self, player_number, id) :
+        self.hp = 2
+        self.atk = 4
+        self.df = 1
+        self.pn = player_number
+        self.id = 'CA' + str(id)
+        self.type = 'C'
+        #self.list_id = None
+        self.other = ['Cruiser', id, 'Ship']
+
+class Destroyer (Ship) :
+    def __init__(self, player_number, id) :
+        self.hp = 1
+        self.atk = 4
+        self.df = 0
+        self.pn = player_number
+        self.id = 'DD' + str(id)
+        self.type = 'D'
+        #self.list_id = None
+        self.other = ['Destroyer', id, 'Ship']
+
+class Dreadnaught (Ship) :
+    def __init__(self, player_number, id) :
+        self.hp = 3
+        self.atk = 6
+        self.df = 3
+        self.pn = player_number
+        self.id = 'DN' + str(id)
+        self.type = 'A'
+        #self.list_id = None
+        self.other = ['Dreadnaught', id, 'Ship']
+
+class Scout (Ship) :
+    def __init__(self, player_number, id) :
+        self.hp = 1
+        self.atk = 3
+        self.df = 0
+        self.pn = player_number
+        self.id = 'SC' + str(id)
+        self.type = 'E'
+        #self.list_id = None
+        self.other = ['Scout', id, 'Ship']
+
+class ShipYard (Ship) :
+    def __init__(self, player_number, id) :
+        self.hp = 1
+        self.atk = 3
+        self.df = 0
+        self.pn = player_number
+        self.id = 'SY' + str(id)
+        self.type = 'C'
+        #self.list_id = None
+        self.other = ['ShipYard', id, 'Yard']
+
+
